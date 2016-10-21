be31beee2200ca2c1af85c3094ceaca448a875d0
Borja <borja.seijo@udc.es>
2016-10-21 16:14:24 +0200

    Fixed bug in svd imputation method

--- src/matlab/missing_data/imputation/imputation.m
+++ src/matlab/missing_data/imputation/imputation.m
@@ -14,25 +14,26 @@ function [D_mcar Dt_mcar Dv_mcar] = imputation(imp_method, D, Dt, Dv, M_mcar, Mt
     switch(imp_method)
         case 'median' % imputation by median value.
             for i=1:size(X_mcar,2)
-                X_median = [X_median median(X_mcar((find(M_mcar(:,i) == 1))))];
-                Xt_median = [Xt_median median(Xt_mcar((find(Mt_mcar(:,i) == 1))))];
-                Xv_median = [Xv_median median(Xv_mcar((find(Mv_mcar(:,i) == 1))))];
+                X_median = [X_median median(X_mcar(~M_mcar(:,i)))];
+                Xt_median = [Xt_median median(Xt_mcar(~Mt_mcar(:,i)))];
+                Xv_median = [Xv_median median(Xv_mcar(~Mv_mcar(:,i)))];
             end
-            D_mcar.X = (D_mcar.X .* M_mcar) + (~M_mcar .* repmat(X_median,size(D_mcar.X,1),1));
-            Dt_mcar.X = (Dt_mcar.X .* Mt_mcar) + (~Mt_mcar .* repmat(Xt_median,size(Dt_mcar.X,1),1));
-            Dv_mcar.X = (Dv_mcar.X .* Mv_mcar) + (~Mv_mcar .* repmat(Xv_median,size(Dv_mcar.X,1),1));
+            D_mcar.X = (D_mcar.X .* ~M_mcar) + (M_mcar .* repmat(X_median,size(D_mcar.X,1),1));
+            Dt_mcar.X = (Dt_mcar.X .* ~Mt_mcar) + (Mt_mcar .* repmat(Xt_median,size(Dt_mcar.X,1),1));
+            Dv_mcar.X = (Dv_mcar.X .* ~Mv_mcar) + (Mv_mcar .* repmat(Xv_median,size(Dv_mcar.X,1),1));
         case 'svd' % imputation by svd
             maxiter = 10;
+            maxnum = 10;
             X_mcar(M_mcar) = 0;
             Xt_mcar(Mt_mcar) = 0;
             Xv_mcar(Mv_mcar) = 0;
             for k=1:maxiter
-                [U, S, V] = svds(X_mcar);
-                [Ut, St, Vt] = svds(Xt_mcar);
-                [Uv, Sv, Vv] = svds(Xv_mcar);
-                XX = U*S*V;
-                XXt = Ut*St*Vt;
-                XXv = Uv*Sv*Vv;
+                [U, S, V] = svds(X_mcar, maxnum);
+                [Ut, St, Vt] = svds(Xt_mcar, maxnum);
+                [Uv, Sv, Vv] = svds(Xv_mcar, maxnum);
+                XX = U*S*V';
+                XXt = Ut*St*Vt';
+                XXv = Uv*Sv*Vv';
                 X_mcar(M_mcar) = XX(M_mcar);
                 Xt_mcar(Mt_mcar) = XXt(Mt_mcar);
                 Xv_mcar(Mv_mcar) = XXv(Mv_mcar);
--- src/matlab/missing_data/missingness/mcar.m
+++ src/matlab/missing_data/missingness/mcar.m
@@ -13,12 +13,15 @@ function [ M_mcar Mt_mcar Mv_mcar ] = mcar( mcar_method, mcar_percent, D, Dt, Dv
             miss_size = round(total_size*(mcar_percent/100));
             rank_v = randperm(total_size);
             miss_pos = rank_v(1:miss_size);
-            Dmiss = ones(x+y+z,n)';
-            Dmiss(miss_pos)=0;
+            Dmiss = zeros(x+y+z,n)';
+            Dmiss(miss_pos)=1;
             Dmiss = Dmiss';
             M_mcar = Dmiss(1:x,:);
             Mv_mcar = Dmiss((x+1):(x+y),:);
             Mt_mcar = Dmiss((x+y+1):end,:);
     end
+    M_mcar = logical(M_mcar);
+    Mt_mcar = logical(Mt_mcar);
+    Mv_mcar = logical(Mv_mcar);
 end
 
