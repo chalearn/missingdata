5eab74d39cd650abfd98910c0f1a732323790bc3
Borja <borja.seijo@udc.es>
2016-10-26 16:53:31 +0200

    Added recursively to the restore missingnes function. Now the function restore all the different missinges datasets in a folder

--- src/matlab/main/restore_dataset.m
+++ src/matlab/main/restore_dataset.m
@@ -30,36 +30,55 @@ function [ output_args ] = restore_( dataset_name, imputation_method )
     % Create a folder to save the different missing datasets.
     data_dest_folder = [datadir filesep dataset_dest_folder filesep dataset_name];
     mkdir(data_dest_folder);
+    
+    aux_folds = dir(data_orig_folder);
+    miss_type_fold = aux_folds(3:end);
+    for t=1:length(miss_type_fold)
+        type_orig_fold = [data_orig_folder filesep miss_type_fold(t).name];
+        type_dest_fold = [data_dest_folder filesep miss_type_fold(t).name];
+        mkdir(type_dest_fold);
+        aux_folds = dir(type_orig_fold);
+        miss_method_fold = aux_folds(3:end);
+        for m=1:length(miss_method_fold)
+            method_orig_fold = [type_orig_fold filesep miss_method_fold(m).name];
+            method_dest_fold = [type_dest_fold filesep miss_method_fold(m).name];
+            mkdir(method_dest_fold);
+            aux_folds = dir(method_orig_fold);
+            miss_perc_fold = aux_folds(3:end);
+            for p=1:length(miss_perc_fold)
+                if (~strcmp(miss_perc_fold(p).name, '0'))
+                    perc_orig_fold = [method_orig_fold filesep miss_perc_fold(p).name];
+                    perc_dest_fold = [method_dest_fold filesep miss_perc_fold(p).name];
+                    mkdir(perc_dest_fold);
 
-    %CAMBIAR PARA HACER AUTOMATICO Y MULTICARPETA
-    data_nomiss_folder = [data_orig_folder filesep 'mcar' filesep 'flipcoin' filesep '0'];
-    % Load the original dataset, divided in train, test, validation, ...
-    [D Dt Dv F T] = load_dataset(data_nomiss_folder, data_train_name, data_test_name, ...
-                                 data_valid_name, data_feat_name);
-    data_miss_folder = [data_orig_folder filesep 'mcar' filesep 'flipcoin' filesep '10'];
-    % Load the missing dataset, divided in train, test, validation, ...
-    [D_m Dt_m Dv_m F_m T_m] = load_dataset(data_miss_folder, data_train_name, data_test_name, ...
-                                 data_valid_name, data_feat_name);
+                    % Load the missing dataset, divided in train, test, validation, ...
+                    [D_m Dt_m Dv_m F_m T_m] = ...
+                                load_dataset(perc_orig_fold, data_train_name, ...
+                                            data_test_name, data_valid_name, ...
+                                            data_feat_name);
                              
-    for i=1:length(imputation_method)
-        data_rest_folder = ...
-                    [data_dest_folder filesep 'mcar' filesep 'flipcoin' filesep '10' filesep imputation_method{i}];
-        mkdir(data_rest_folder);
-        % Apply an imputation over the missing data values.
-        [D_r Dt_r Dv_r] = imputation(imputation_method{i}, D_m, Dt_m, Dv_m);
-        % Save the train data to the files.
-        dlmwrite([data_rest_folder filesep dataset_name '_train.data'], D_r.X, ' ');
-        dlmwrite([data_rest_folder filesep dataset_name '_train.labels'], D_r.Y, ' ');
-        % Save the test data to the files.
-        dlmwrite([data_rest_folder filesep dataset_name '_test.data'], Dt_r.X, ' ');
-        dlmwrite([data_rest_folder filesep dataset_name '_test.labels'], Dt_r.Y, ' ');
-        % Save the validation data to the files.
-        dlmwrite([data_rest_folder filesep dataset_name '_valid.data'], Dv_r.X, ' ');
-        dlmwrite([data_rest_folder filesep dataset_name '_valid.labels'], Dv_r.Y, ' ');
-        % Save the feat data to the files.
-        fileID = fopen([data_rest_folder filesep dataset_name '_feat.info'],'w');
-        fprintf(fileID,'%s\n', F_m{:});
-        fclose(fileID);
-        dlmwrite([data_rest_folder filesep dataset_name '_feat.labels'], T_m, ' ');
+                    for i=1:length(imputation_method)
+                        data_rest_folder = [perc_dest_fold filesep imputation_method{i}];
+                        mkdir(data_rest_folder);
+                        % Apply an imputation over the missing data values.
+                        [D_r Dt_r Dv_r] = imputation(imputation_method{i}, D_m, Dt_m, Dv_m);
+                        % Save the train data to the files.
+                        dlmwrite([data_rest_folder filesep dataset_name '_train.data'], D_r.X, ' ');
+                        dlmwrite([data_rest_folder filesep dataset_name '_train.labels'], D_r.Y, ' ');
+                        % Save the test data to the files.
+                        dlmwrite([data_rest_folder filesep dataset_name '_test.data'], Dt_r.X, ' ');
+                        dlmwrite([data_rest_folder filesep dataset_name '_test.labels'], Dt_r.Y, ' ');
+                        % Save the validation data to the files.
+                        dlmwrite([data_rest_folder filesep dataset_name '_valid.data'], Dv_r.X, ' ');
+                        dlmwrite([data_rest_folder filesep dataset_name '_valid.labels'], Dv_r.Y, ' ');
+                        % Save the feat data to the files.
+                        fileID = fopen([data_rest_folder filesep dataset_name '_feat.info'],'w');
+                        fprintf(fileID,'%s\n', F_m{:});
+                        fclose(fileID);
+                        dlmwrite([data_rest_folder filesep dataset_name '_feat.labels'], T_m, ' ');
+                    end
+                end
+            end
+        end
     end
 end
