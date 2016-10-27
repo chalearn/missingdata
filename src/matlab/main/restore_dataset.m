896acdfc5b2f10aab826b5256d1203cb6f450d8e
Borja <borja.seijo@udc.es>
2016-10-27 12:26:24 +0200

    Bug fixed in restore dataset function

--- src/matlab/main/restore_dataset.m
+++ src/matlab/main/restore_dataset.m
@@ -46,37 +46,35 @@ function [ output_args ] = restore_( dataset_name, imputation_method )
             aux_folds = dir(method_orig_fold);
             miss_perc_fold = aux_folds(3:end);
             for p=1:length(miss_perc_fold)
-                if (~strcmp(miss_perc_fold(p).name, '0'))
-                    perc_orig_fold = [method_orig_fold filesep miss_perc_fold(p).name];
-                    perc_dest_fold = [method_dest_fold filesep miss_perc_fold(p).name];
-                    mkdir(perc_dest_fold);
+                perc_orig_fold = [method_orig_fold filesep miss_perc_fold(p).name];
+                perc_dest_fold = [method_dest_fold filesep miss_perc_fold(p).name];
+                mkdir(perc_dest_fold);
 
-                    % Load the missing dataset, divided in train, test, validation, ...
-                    [D_m Dt_m Dv_m F_m T_m] = ...
-                                load_dataset(perc_orig_fold, data_train_name, ...
-                                            data_test_name, data_valid_name, ...
-                                            data_feat_name);
-                             
-                    for i=1:length(imputation_method)
-                        data_rest_folder = [perc_dest_fold filesep imputation_method{i}];
-                        mkdir(data_rest_folder);
-                        % Apply an imputation over the missing data values.
-                        [D_r Dt_r Dv_r] = imputation(imputation_method{i}, D_m, Dt_m, Dv_m);
-                        % Save the train data to the files.
-                        dlmwrite([data_rest_folder filesep dataset_name '_train.data'], D_r.X, ' ');
-                        dlmwrite([data_rest_folder filesep dataset_name '_train.labels'], D_r.Y, ' ');
-                        % Save the test data to the files.
-                        dlmwrite([data_rest_folder filesep dataset_name '_test.data'], Dt_r.X, ' ');
-                        dlmwrite([data_rest_folder filesep dataset_name '_test.labels'], Dt_r.Y, ' ');
-                        % Save the validation data to the files.
-                        dlmwrite([data_rest_folder filesep dataset_name '_valid.data'], Dv_r.X, ' ');
-                        dlmwrite([data_rest_folder filesep dataset_name '_valid.labels'], Dv_r.Y, ' ');
-                        % Save the feat data to the files.
-                        fileID = fopen([data_rest_folder filesep dataset_name '_feat.info'],'w');
-                        fprintf(fileID,'%s\n', F_m{:});
-                        fclose(fileID);
-                        dlmwrite([data_rest_folder filesep dataset_name '_feat.labels'], T_m, ' ');
-                    end
+                % Load the missing dataset, divided in train, test, validation, ...
+                [D_m Dt_m Dv_m F_m T_m] = ...
+                            load_dataset(perc_orig_fold, data_train_name, ...
+                                        data_test_name, data_valid_name, ...
+                                        data_feat_name);
+
+                for i=1:length(imputation_method)
+                    data_rest_folder = [perc_dest_fold filesep imputation_method{i}];
+                    mkdir(data_rest_folder);
+                    % Apply an imputation over the missing data values.
+                    [D_r Dt_r Dv_r] = imputation(imputation_method{i}, D_m, Dt_m, Dv_m);
+                    % Save the train data to the files.
+                    dlmwrite([data_rest_folder filesep dataset_name '_train.data'], D_r.X, ' ');
+                    dlmwrite([data_rest_folder filesep dataset_name '_train.labels'], D_r.Y, ' ');
+                    % Save the test data to the files.
+                    dlmwrite([data_rest_folder filesep dataset_name '_test.data'], Dt_r.X, ' ');
+                    dlmwrite([data_rest_folder filesep dataset_name '_test.labels'], Dt_r.Y, ' ');
+                    % Save the validation data to the files.
+                    dlmwrite([data_rest_folder filesep dataset_name '_valid.data'], Dv_r.X, ' ');
+                    dlmwrite([data_rest_folder filesep dataset_name '_valid.labels'], Dv_r.Y, ' ');
+                    % Save the feat data to the files.
+                    fileID = fopen([data_rest_folder filesep dataset_name '_feat.info'],'w');
+                    fprintf(fileID,'%s\n', F_m{:});
+                    fclose(fileID);
+                    dlmwrite([data_rest_folder filesep dataset_name '_feat.labels'], T_m, ' ');
                 end
             end
         end
