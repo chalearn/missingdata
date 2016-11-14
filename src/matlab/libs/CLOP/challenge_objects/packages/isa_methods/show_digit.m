3dd2d1e7636e640ea57581888d239c2e4c3ffe06
Borja <borja.seijo@udc.es>
2016-11-14 10:04:51 +0100

    Changed code in plot function

--- src/matlab/libs/CLOP/challenge_objects/packages/isa_methods/show_digit.m
+++ src/matlab/libs/CLOP/challenge_objects/packages/isa_methods/show_digit.m
@@ -18,11 +18,11 @@ end
 
 imn = uint8(imn*(num-1));
 colormap(map);
+hold on
 image(imn); 
 if (nargin>1)
-    hold on
     [x_miss y_miss] = find(imiss);
-    plot(y_miss, x_miss, [color '.'],'MarkerSize', 25);
-    hold off
+    plot(y_miss, x_miss, [color '.'],'MarkerSize', 20);
 end
+hold off
 warning on
