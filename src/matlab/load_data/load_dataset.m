6c0b607264dddf42bbe09e80a70a7703f7dc1474
Borja Seijo <borja.seijo.p@gmail.com>
2017-09-29 14:51:42 +0100

    Added comments to the function and changed order of variables

--- src/matlab/load_data/load_dataset.m
+++ src/matlab/load_data/load_dataset.m
@@ -1,30 +1,54 @@
-function [ D Dt Dv F T ] = load_dataset( folder_data_root, f_train_name, ...
-                                        f_test_name, f_valid_name, f_probes_name )
-%LOAD_TRAIN_VALID_TEST Summary of this function goes here
-%   Detailed explanation goes here
+function [ D, Dv, Dt, F, T, error_ld ] = load_dataset( fold_root_data, ...
+                                    file_name_train, file_name_valid, ...
+                                    file_name_test, file_name_probes )
+%LOAD_DATASET Load the dataset files into Matlab variables.
+% INPUT:
+%   fold_root_data:     Path of the dataset root folder.
+%   file_name_train:    Name of the train file.
+%   file_name_valid:    Name of the validation file.
+%   file_name_test:     Name of the test file.
+%   file_name_probes:   Name of the probes file.
+% OUTPUT:
+%   D:          Data type that represents the dataset that will be classified.
+%   Dv:         Data type that represents the dataset that will be used to
+%               perform the validation.
+%   Dt:         Data type that represents the dataset that will be used to
+%               perform the test.
+%   F:          
+%   T:          Array of target values.
+%   error_ld:   Possible error when the function is executed:
+%                   0 - No error.
+%                   1 - Incorrect number of parameters.
 
+% Set the initial value of return variables.
+D = [];
+Dv = [];
+Dt = [];
 F = [];
 T = [];
+error_ld = 0;
 
-if nargin >= 2
-    X=load([folder_data_root filesep f_train_name '.data']);
-    Y=load([folder_data_root filesep f_train_name '.labels']);
-    D = data (X, Y);
+% Check the number of parameters.
+if (nargin<2)
+    error_ld = 1;
+else
+    if (nargin>1)
+        X=load([fold_root_data filesep file_name_train '.data']);
+        Y=load([fold_root_data filesep file_name_train '.labels']);
+        D = data (X, Y);
+    end
+    if (nargin>2)
+        Xv=load([fold_root_data filesep file_name_valid '.data']);
+        Yv=load([fold_root_data filesep file_name_valid '.labels']);
+        Dv = data (Xv, Yv);
+    end
+    if (nargin>3)
+        Xt=load([fold_root_data filesep file_name_test '.data']);
+        Yt=load([fold_root_data filesep file_name_test '.labels']);
+        Dt = data (Xt, Yt);
+    end
+    if (nargin>4)
+        F=read_label([fold_root_data filesep file_name_probes '.info']); % Know the identity of the probes
+        T=load([fold_root_data filesep file_name_probes '.labels']); % +1 for a real feature; -1 for a probe
+    end
 end
-if nargin >= 3
-    Xt=load([folder_data_root filesep f_test_name '.data']);
-    Yt=load([folder_data_root filesep f_test_name '.labels']);
-    Dt = data (Xt, Yt);
-end
-if nargin >= 4
-    Xv=load([folder_data_root filesep f_valid_name '.data']);
-    Yv=load([folder_data_root filesep f_valid_name '.labels']);
-    Dv = data (Xv, Yv);
-end
-if nargin >= 5
-    F=read_label([folder_data_root filesep f_probes_name '.info']); % Know the identity of the probes
-    T=load([folder_data_root filesep f_probes_name '.labels']); % +1 for a real feature; -1 for a probe
-end
-
-end
-
