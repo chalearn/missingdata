817b24aa288499cfd49526f1b0d759c94c20832c
Borja Seijo <borja.seijo.p@gmail.com>
2017-10-02 16:35:59 +0100

    Changed folder name and added new methods to deal with missing data

---
+++ src/matlab/solution/deletion.m
@@ -0,0 +1,7 @@
+function [ output_args ] = deletion( input_args )
+%DELETION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ src/matlab/solution/solve_missing.m
@@ -0,0 +1,6 @@
+function [ output_args ] = solve_missing( input_args )
+
+
+
+end
+
